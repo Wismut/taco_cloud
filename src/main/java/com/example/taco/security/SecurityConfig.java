7c742949b79dddff900b50cea42a1c7cca16e884
Wismut <wismutmax@gmail.com>
2020-02-23 14:31:49 +0200

    Implement registration

--- src/main/java/com/example/taco/security/SecurityConfig.java
+++ src/main/java/com/example/taco/security/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.taco.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 import javax.sql.DataSource;
@@ -15,17 +18,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     DataSource dataSource;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new StandardPasswordEncoder("53cr3t");
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery(
-                        "select username, password, enabled from Users " +
-                                "where username=?")
-                .authoritiesByUsernameQuery(
-                        "select username, authority from UserAuthorities " +
-                                "where username=?")
-        .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+                .userDetailsService(userDetailsService)
+        .passwordEncoder(encoder());
+//        auth
+//                .jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery(
+//                        "select username, password, enabled from Users " +
+//                                "where username=?")
+//                .authoritiesByUsernameQuery(
+//                        "select username, authority from UserAuthorities " +
+//                                "where username=?")
+//        .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
     }
 }
