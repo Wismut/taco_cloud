5c989827b633dea04c8e0485a1ecf04bdb1b8c59
Sergiy <asd@sed.dfs>
2020-05-28 19:14:09 +0300

    Implements DesignTacoController api

--- src/main/java/com/example/taco/data/TacoRepository.java
+++ src/main/java/com/example/taco/data/TacoRepository.java
@@ -1,8 +1,11 @@
 package com.example.taco.data;
 
 import com.example.taco.Taco;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TacoRepository extends CrudRepository<Taco, Long> {
     Taco save(Taco design);
+
+    Iterable<Taco> findAll(Pageable page);
 }
---
+++ src/main/java/com/example/taco/web/api/DesignTacoController.java
@@ -0,0 +1,41 @@
+package com.example.taco.web.api;
+
+import com.example.taco.Taco;
+import com.example.taco.data.TacoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/design",
+        produces = "application/json")
+@CrossOrigin(origins = "*")
+public class DesignTacoController {
+    private TacoRepository tacoRepo;
+    @Autowired
+    EntityLinks entityLinks;
+
+    public DesignTacoController(TacoRepository tacoRepo) {
+        this.tacoRepo = tacoRepo;
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(
+                0, 12, Sort.by("createdAt").descending());
+        return tacoRepo.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optTaco = tacoRepo.findById(id);
+        return optTaco.map(taco -> new ResponseEntity<>(taco, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
