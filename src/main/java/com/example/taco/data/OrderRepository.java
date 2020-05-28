2042ff1e7f747fc9cd9d5fc5be472ea759ee7f0c
Sergiy <asd@sed.dfs>
2020-05-28 10:04:17 +0300

    Add pagination for orders

--- src/main/java/com/example/taco/data/OrderRepository.java
+++ src/main/java/com/example/taco/data/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.example.taco.data;
 
 import com.example.taco.Order;
+import com.example.taco.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,4 +11,6 @@ public interface OrderRepository extends CrudRepository<Order, Long> {
     Order save(Order order);
 
     List<Order> findByZip(String deliveryZip);
+
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
--- src/main/java/com/example/taco/web/OrderController.java
+++ src/main/java/com/example/taco/web/OrderController.java
@@ -5,7 +5,12 @@ import com.example.taco.User;
 import com.example.taco.data.OrderRepository;
 import com.example.taco.data.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,13 +25,19 @@ import java.security.Principal;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
+@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
     private OrderRepository orderRepo;
     private UserRepository userRepository;
+    private int pageSize = 20;
 
     public OrderController(OrderRepository orderRepo, UserRepository userRepository) {
         this.orderRepo = orderRepo;
         this.userRepository = userRepository;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
     }
 
     @GetMapping("/current")
@@ -48,5 +59,14 @@ public class OrderController {
         orderRepo.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(
+            @AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, pageSize);
+        model.addAttribute("orders",
+                orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 }
