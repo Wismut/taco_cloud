472cd95e865c68cc1ac75f39957515f0f14f29de
Sergiy <asd@sed.dfs>
2020-05-29 16:05:47 +0300

    Implements resource handling

--- src/main/java/com/example/taco/web/WebConfig.java
+++ src/main/java/com/example/taco/web/WebConfig.java
@@ -1,6 +1,12 @@
 package com.example.taco.web;
 
+import com.example.taco.Taco;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +16,21 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/login");
+    }
+
+    @Bean
+    public ResourceProcessor<PagedResources<Resource<Taco>>>
+    tacoProcessor(EntityLinks links) {
+        return new ResourceProcessor<PagedResources<Resource<Taco>>>() {
+            @Override
+            public PagedResources<Resource<Taco>> process(
+                    PagedResources<Resource<Taco>> resource) {
+                resource.add(
+                        links.linkFor(Taco.class)
+                                .slash("recent")
+                                .withRel("recents"));
+                return resource;
+            }
+        };
     }
 }
