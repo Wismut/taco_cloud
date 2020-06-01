83d6cfff3f822c8674992dc45557f08ef5a35aa7
Sergiy <asd@sed.dfs>
2020-06-01 17:27:00 +0300

    Add default destination for jms

--- src/main/java/com/example/taco/messaging/JmsOrderMessagingService.java
+++ src/main/java/com/example/taco/messaging/JmsOrderMessagingService.java
@@ -3,12 +3,8 @@ package com.example.taco.messaging;
 import com.example.taco.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
 
 @Service
 public class JmsOrderMessagingService implements OrderMessagingService {
@@ -21,13 +17,6 @@ public class JmsOrderMessagingService implements OrderMessagingService {
 
     @Override
     public void sendOrder(Order order) {
-        jms.send(new MessageCreator() {
-                     @Override
-                     public Message createMessage(Session session)
-                             throws JMSException {
-                         return session.createObjectMessage(order);
-                     }
-                 }
-        );
+        jms.send(session -> session.createObjectMessage(order));
     }
 }
