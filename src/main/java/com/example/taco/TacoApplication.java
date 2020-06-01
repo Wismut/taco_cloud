082071835a54819a8ed4b92db98aecaf50b71810
Sergiy <asd@sed.dfs>
2020-06-01 17:20:00 +0300

    Add Artemis dependency Implements JmsOrderMessagingService

--- src/main/java/com/example/taco/TacoApplication.java
+++ src/main/java/com/example/taco/TacoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TacoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TacoApplication.class, args);
     }
