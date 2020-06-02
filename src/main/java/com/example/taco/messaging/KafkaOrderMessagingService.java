ef4d85d33dd0cfb692be5d85fe03108f12cf6010
Sergiy <asd@sed.dfs>
2020-06-02 12:53:55 +0300

    Implements KafkaOrderMessagingService

---
+++ src/main/java/com/example/taco/messaging/KafkaOrderMessagingService.java
@@ -0,0 +1,25 @@
+package com.example.taco.messaging;
+
+import com.example.taco.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaOrderMessagingService
+        implements OrderMessagingService {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaOrderMessagingService(
+            KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void sendOrder(Order order) {
+        kafkaTemplate.send("tacocloud.orders.topic",
+                order.getId().toString(),
+                order.toString());
+    }
+}
