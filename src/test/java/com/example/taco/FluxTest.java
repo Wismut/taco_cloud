0036c2847e92f026449a39f78cec2d8d37451ae3
Sergiy <asd@sed.dfs>
2020-06-10 18:53:25 +0300

    Implements Flux tests

---
+++ src/test/java/com/example/taco/FluxTest.java
@@ -0,0 +1,64 @@
+package com.example.taco;
+
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+
+public class FluxTest {
+    @Test
+    public void createAFlux_just() {
+        Flux<String> fruitFlux = Flux
+                .just("Apple", "Orange", "Grape", "Banana", "Strawberry");
+        StepVerifier.create(fruitFlux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+
+    @Test
+    public void createAFlux_fromArray() {
+        String[] fruits = new String[]{
+                "Apple", "Orange", "Grape", "Banana", "Strawberry"};
+        Flux<String> fruitFlux = Flux.fromArray(fruits);
+        StepVerifier.create(fruitFlux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+
+    @Test
+    public void createAFlux_range() {
+        Flux<Integer> intervalFlux =
+                Flux.range(1, 5);
+        StepVerifier.create(intervalFlux)
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .expectNext(5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void createAFlux_interval() {
+        Flux<Long> intervalFlux =
+                Flux.interval(Duration.ofSeconds(1))
+                        .take(5);
+        StepVerifier.create(intervalFlux)
+                .expectNext(0L)
+                .expectNext(1L)
+                .expectNext(2L)
+                .expectNext(3L)
+                .expectNext(4L)
+                .verifyComplete();
+    }
+}
