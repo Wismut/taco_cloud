87bd61d42234739384258861ccb80dbade8d1c3d
Sergiy <asd@sed.dfs>
2020-06-11 09:02:07 +0300

    Implements Flux tests

--- src/test/java/com/example/taco/FluxTest.java
+++ src/test/java/com/example/taco/FluxTest.java
@@ -3,6 +3,7 @@ package com.example.taco;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
 
 import java.time.Duration;
 
@@ -59,6 +60,107 @@ public class FluxTest {
                 .expectNext(2L)
                 .expectNext(3L)
                 .expectNext(4L)
+                .verifyComplete();
+    }
+
+    @Test
+    public void mergeFluxes() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa")
+                .delayElements(Duration.ofMillis(500));
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples")
+                .delaySubscription(Duration.ofMillis(250))
+                .delayElements(Duration.ofMillis(500));
+        Flux<String> mergedFlux = characterFlux.mergeWith(foodFlux);
+        StepVerifier.create(mergedFlux)
+                .expectNext("Garfield")
+                .expectNext("Lasagna")
+                .expectNext("Kojak")
+                .expectNext("Lollipops")
+                .expectNext("Barbossa")
+                .expectNext("Apples")
+                .verifyComplete();
+    }
+
+    @Test
+    public void zipFluxes() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa");
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+        Flux<Tuple2<String, String>> zippedFlux =
+                Flux.zip(characterFlux, foodFlux);
+        StepVerifier.create(zippedFlux)
+                .expectNextMatches(p ->
+                        p.getT1().equals("Garfield") &&
+                                p.getT2().equals("Lasagna"))
+                .expectNextMatches(p ->
+                        p.getT1().equals("Kojak") &&
+                                p.getT2().equals("Lollipops"))
+                .expectNextMatches(p ->
+                        p.getT1().equals("Barbossa") &&
+                                p.getT2().equals("Apples"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void zipFluxesToObject() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa");
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+        Flux<String> zippedFlux =
+                Flux.zip(characterFlux, foodFlux, (c, f) -> c + " eats " + f);
+        StepVerifier.create(zippedFlux)
+                .expectNext("Garfield eats Lasagna")
+                .expectNext("Kojak eats Lollipops")
+                .expectNext("Barbossa eats Apples")
+                .verifyComplete();
+    }
+
+    @Test
+    public void firstFlux() {
+        Flux<String> slowFlux = Flux.just("tortoise", "snail", "sloth")
+                .delaySubscription(Duration.ofMillis(100));
+        Flux<String> fastFlux = Flux.just("hare", "cheetah", "squirrel");
+        Flux<String> firstFlux = Flux.first(slowFlux, fastFlux);
+        StepVerifier.create(firstFlux)
+                .expectNext("hare")
+                .expectNext("cheetah")
+                .expectNext("squirrel")
+                .verifyComplete();
+    }
+
+    @Test
+    public void skipAFew() {
+        Flux<String> skipFlux = Flux.just(
+                "one", "two", "skip a few", "ninety nine", "one hundred")
+                .skip(3);
+        StepVerifier.create(skipFlux)
+                .expectNext("ninety nine", "one hundred")
+                .verifyComplete();
+    }
+
+    @Test
+    public void skipAFewSeconds() {
+        Flux<String> skipFlux = Flux.just(
+                "one", "two", "skip a few", "ninety nine", "one hundred")
+                .delayElements(Duration.ofSeconds(1))
+                .skip(Duration.ofSeconds(4));
+        StepVerifier.create(skipFlux)
+                .expectNext("ninety nine", "one hundred")
+                .verifyComplete();
+    }
+
+    @Test
+    public void take() {
+        Flux<String> nationalParkFlux = Flux.just(
+                "Yellowstone", "Yosemite", "Grand Canyon",
+                "Zion", "Grand Teton")
+                .take(3);
+        StepVerifier.create(nationalParkFlux)
+                .expectNext("Yellowstone", "Yosemite", "Grand Canyon")
                 .verifyComplete();
     }
 }
