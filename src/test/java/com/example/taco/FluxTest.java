c6bcadcb86ddf77040d014980535c1c860412aa5
Sergiy <asd@sed.dfs>
2020-06-11 13:35:47 +0300

    Implements tests for all and any

--- src/test/java/com/example/taco/FluxTest.java
+++ src/test/java/com/example/taco/FluxTest.java
@@ -277,4 +277,32 @@ public class FluxTest {
                         map.get('k').equals("kangaroo"))
                 .verifyComplete();
     }
+
+    @Test
+    public void all() {
+        Flux<String> animalFlux = Flux.just(
+                "aardvark", "elephant", "koala", "eagle", "kangaroo");
+        Mono<Boolean> hasAMono = animalFlux.all(a -> a.contains("a"));
+        StepVerifier.create(hasAMono)
+                .expectNext(true)
+                .verifyComplete();
+        Mono<Boolean> hasKMono = animalFlux.all(a -> a.contains("k"));
+        StepVerifier.create(hasKMono)
+                .expectNext(false)
+                .verifyComplete();
+    }
+
+    @Test
+    public void any() {
+        Flux<String> animalFlux = Flux.just(
+                "aardvark", "elephant", "koala", "eagle", "kangaroo");
+        Mono<Boolean> hasAMono = animalFlux.any(a -> a.contains("t"));
+        StepVerifier.create(hasAMono)
+                .expectNext(true)
+                .verifyComplete();
+        Mono<Boolean> hasZMono = animalFlux.any(a -> a.contains("z"));
+        StepVerifier.create(hasZMono)
+                .expectNext(false)
+                .verifyComplete();
+    }
 }
