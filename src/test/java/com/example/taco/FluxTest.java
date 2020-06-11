67870272d44bef89b888ed5d675d79a9a22fb4f3
Sergiy <asd@sed.dfs>
2020-06-11 11:11:14 +0300

    Add tests for flux

--- src/test/java/com/example/taco/FluxTest.java
+++ src/test/java/com/example/taco/FluxTest.java
@@ -10,6 +10,7 @@ import reactor.util.function.Tuple2;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 
 public class FluxTest {
@@ -235,6 +236,45 @@ public class FluxTest {
                 .expectNextMatches(playerList::contains)
                 .expectNextMatches(playerList::contains)
                 .expectNextMatches(playerList::contains)
+                .verifyComplete();
+    }
+
+    @Test
+    public void buffer() {
+        Flux<String> fruitFlux = Flux.just(
+                "apple", "orange", "banana", "kiwi", "strawberry");
+        Flux<List<String>> bufferedFlux = fruitFlux.buffer(3);
+        StepVerifier
+                .create(bufferedFlux)
+                .expectNext(Arrays.asList("apple", "orange", "banana"))
+                .expectNext(Arrays.asList("kiwi", "strawberry"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void collectList() {
+        Flux<String> fruitFlux = Flux.just(
+                "apple", "orange", "banana", "kiwi", "strawberry");
+        Mono<List<String>> fruitListMono = fruitFlux.collectList();
+        StepVerifier
+                .create(fruitListMono)
+                .expectNext(Arrays.asList(
+                        "apple", "orange", "banana", "kiwi", "strawberry"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void collectMap() {
+        Flux<String> animalFlux = Flux.just(
+                "aardvark", "elephant", "koala", "eagle", "kangaroo");
+        Mono<Map<Character, String>> animalMapMono =
+                animalFlux.collectMap(a -> a.charAt(0));
+        StepVerifier
+                .create(animalMapMono)
+                .expectNextMatches(map -> map.size() == 3 &&
+                        map.get('a').equals("aardvark") &&
+                        map.get('e').equals("eagle") &&
+                        map.get('k').equals("kangaroo"))
                 .verifyComplete();
     }
 }
