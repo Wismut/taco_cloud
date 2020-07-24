bae6ea86aa284fc629b03058dac904ada854bc35
Sergiy <asd@sed.dfs>
2020-07-24 22:28:28 +0300

    Implements DesignTacoControllerWebTest

--- src/test/java/com/example/taco/DesignTacoControllerTest.java
+++ src/test/java/com/example/taco/DesignTacoControllerTest.java
@@ -1,15 +1,19 @@
 package com.example.taco;
 
+import com.example.taco.data.IngredientRepository;
 import com.example.taco.data.TacoRepository;
 import com.example.taco.web.DesignTacoController;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 public class DesignTacoControllerTest {
@@ -44,6 +48,27 @@ public class DesignTacoControllerTest {
                 .jsonPath("$[11].name").isEqualTo("Taco 12").jsonPath("$[12]")
                 .doesNotExist()
                 .jsonPath("$[12]").doesNotExist();
+    }
+
+    @Test
+    public void shouldSaveATaco() {
+        TacoRepository tacoRepo = Mockito.mock(TacoRepository.class);
+        IngredientRepository ingredientRepository = Mockito.mock(IngredientRepository.class);
+        Mono<Taco> unsavedTacoMono = Mono.just(testTaco(null));
+        Taco savedTaco = testTaco(null);
+        savedTaco.setId(1L);
+        Mono<Taco> savedTacoMono = Mono.just(savedTaco);
+        when(tacoRepo.save((Mono<Taco>) any())).thenReturn(savedTacoMono);
+        WebTestClient testClient = WebTestClient.bindToController(
+                new DesignTacoController(ingredientRepository, tacoRepo)).build();
+        testClient.post()
+                .uri("/design")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(unsavedTacoMono, Taco.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Taco.class)
+                .isEqualTo(savedTaco);
     }
 
     private Taco testTaco(Long number) {
