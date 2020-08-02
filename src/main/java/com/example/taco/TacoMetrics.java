ac1882773bc156630f7a670abbbdb097a44648a7
Sergiy <asd@sed.dfs>
2020-08-02 13:31:30 +0300

    Implements TacoMetrics

---
+++ src/main/java/com/example/taco/TacoMetrics.java
@@ -0,0 +1,25 @@
+package com.example.taco;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.data.rest.core.event.AbstractRepositoryEventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class TacoMetrics extends AbstractRepositoryEventListener<Taco> {
+    private final MeterRegistry meterRegistry;
+
+    public TacoMetrics(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
+    @Override
+    protected void onAfterCreate(Taco taco) {
+        List<Ingredient> ingredients = taco.getIngredients();
+        for (Ingredient ingredient : ingredients) {
+            meterRegistry.counter("tacoCloud",
+                    "ingredient", ingredient.getId()).increment();
+        }
+    }
+}
